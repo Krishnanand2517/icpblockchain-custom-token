1e18ba82ad593a7f578f5f1a82318321e1543ff5
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-12-08 13:15:36 +0530

    feat: Create faucet functionality to grant 10,000 tokens to first-time users

--- src/token_backend/main.mo
+++ src/token_backend/main.mo
@@ -1,5 +1,6 @@
 import Principal "mo:base/Principal";
 import HashMap "mo:base/HashMap";
+import Debug "mo:base/Debug";
 
 actor Token {
   var owner : Principal = Principal.fromText("4q423-c33lr-qp4gu-wf3kt-3k3bv-qnj5y-3ttbv-hvyo5-mbfak-vf57e-cqe");
@@ -20,5 +21,17 @@ actor Token {
 
   public query func getSymbol() : async Text {
     return symbol;
+  };
+
+  public shared (msg) func payOut() : async Text {
+    Debug.print(debug_show (msg.caller));
+
+    if (balances.get(msg.caller) == null) {
+      let amount = 10000;
+      balances.put(msg.caller, amount);
+      return "Success";
+    } else {
+      return "Already Claimed";
+    };
   };
 };
--- src/token_frontend/src/components/Faucet.jsx
+++ src/token_frontend/src/components/Faucet.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+
+import {token_backend} from "../../../declarations/token_backend";
 
 function Faucet() {
+  const [isDisabled, setIsDisabled] = useState(false)
+  const [buttonText, setButtonText] = useState("Gimme gimme")
 
   async function handleClick(event) {
-
+    setIsDisabled(true);
+    const result = await token_backend.payOut();
+    setButtonText(result);
   }
 
   return (
@@ -16,8 +22,8 @@ function Faucet() {
       </h2>
       <label>Get your free DKrish tokens here! Claim 10,000 KRIS coins to your account.</label>
       <p className="trade-buttons">
-        <button id="btn-payout" onClick={handleClick}>
-          Gimme gimme
+        <button id="btn-payout" onClick={handleClick} disabled={isDisabled}>
+          {buttonText}
         </button>
       </p>
     </div>
