5fd3c00c449609f442edc6df141412b69ddaf39a
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-12-08 12:47:05 +0530

    feat: Get current balance from Principal ID in frontend

--- src/token_frontend/src/components/Balance.jsx
+++ src/token_frontend/src/components/Balance.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+import { Principal } from "@dfinity/principal";
+
+import {token_backend} from '../../../declarations/token_backend';
 
 function Balance() {
+  const [inputValue, setInputValue] = useState("");
+  const [balance, setBalance] = useState("");
+  const [currencySymbol, setCurrencySymbol] = useState("");
   
   async function handleClick() {
-    console.log("Balance Button Clicked");
-  }
+    const principal = Principal.fromText(inputValue);
+    const balanceResult = await token_backend.balanceOf(principal);
+    const symbolResult = await token_backend.getSymbol();
 
+    setBalance(balanceResult.toLocaleString());
+    setCurrencySymbol(symbolResult);
+  }
 
   return (
     <div className="window white">
@@ -15,6 +25,8 @@ function Balance() {
           id="balance-principal-id"
           type="text"
           placeholder="Enter a Principal ID"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
         />
       </p>
       <p className="trade-buttons">
@@ -25,7 +37,7 @@ function Balance() {
           Check Balance
         </button>
       </p>
-      <p>This account has a balance of XYZ.</p>
+      <p hidden={balance === ""}>This account has a balance of {balance} {currencySymbol}.</p>
     </div>
   );
 }
