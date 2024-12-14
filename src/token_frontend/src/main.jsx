c34b607aa741d47977d15654d9c83752062b6204
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-12-14 12:14:46 +0530

    feat: Add authentication to open the app

--- src/token_frontend/src/main.jsx
+++ src/token_frontend/src/main.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { AuthClient } from '@dfinity/auth-client';
 import App from './components/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const init = async () => {
+  const authClient = await AuthClient.create();
+
+  if (await authClient.isAuthenticated()) {
+    handleAuthenticated();
+  } else {
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () => {
+        handleAuthenticated();
+      }
+    });
+  }
+};
+
+const handleAuthenticated = async (authClient) => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
+
+init();
